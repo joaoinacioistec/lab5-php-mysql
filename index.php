77bc20f6a2b074f1771975d6faeaacdbe684acc7
joaoinacioistec <161620485+joaoinacioistec@users.noreply.github.com>
2024-04-30 10:29:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php echo("Hello World"); ?>
+<?php echo("Hello World"); 
+$server = 'sqlbd-lab5.database.windows.net';
+$dbName = 'sqldb-lab5';
+$uid = 'sqladmin';
+$pwd = 'Omopla1@';
+
+$conn = new PDO("sqlsrv:server=$server; database = $dbName", $uid, $pwd);
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+
+try {
+    $tableName = 'dbo.Users';
+    $query = "SELECT * FROM $tableName";
+    $stmt = $conn->query($query);
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($result);
+    
+    unset($stmt);
+    unset($conn);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
